00d39a941a6d386a4291f03ce3f0a8a63dd693bd
songyejin <yjsong0088@gmail.com>
2024-08-01 08:36:36 +0000

    tt

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 
 @app.get("/health")async def health_check():
     return PlainTextResponse("건강합니다", status_code=200)
+
+@app.get("/")
+async def health_check1():
+    return PlainTextResponse("CI/CD가 성공했습니다",status_code=200)
+
